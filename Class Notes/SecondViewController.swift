56f705c9d5e010fe989cedf605978e6ec80a1459
Annahlu Raclawski <annahlu47@gmail.com>
2023-10-13 09:42:15 -0500

    In class October 13

---
+++ Class Notes/SecondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SecondViewController.swift
+//  Class Notes
+//
+//  Created by ANNAHLU RACLAWSKI on 10/6/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var stu : Student!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(stu.name)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Class Notes/ViewController.swift
+++ Class Notes/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Building a blank student array, giving initial value
+    var students : [Student] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let s1 = Student(name: "Bob", age: 10, money: 4.23)
+        let s2 = Student(name: "Bill", age: 11, money: 8.46)
+        
+        students.append(s1)
+        students.append(s2)
+        students.append(Student(name: "Jack", age: 12, money: 16.92))
+        
+        print(students[1].name)
+        students[1].name = "Will"
+        print(students[1].name)
+        
+        students[1].deposit(moneyIn: 8.54)
+        print(students[1].money)
+        
+        
+        let struct1 = StudentStruct(name: "Bobby", age: 34, money: 4000.00, rating: Rating.fire)
+        print(struct1.name)
+        switch struct1.rating {
+        case Rating.fire:
+            print("You are fire")
+        case Rating.mid:
+            print("You are mid")
+        case Rating.trash:
+            print("You are trash")
+        default:
+            print("You are nothing")
+        }
     }
 
-
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "first", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nvc = segue.destination as! SecondViewController
+        
+        nvc.stu = students[0]
+    }
 }
 
